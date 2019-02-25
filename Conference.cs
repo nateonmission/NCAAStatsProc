b2208db6fae8edd599f7093873a32b9139ee3467
J. Nathan Allen <nateonmission@gmail.com>
2019-02-25 18:51:31 -0500

    Conf fields pt2

--- Conference.cs
+++ Conference.cs
@@ -10,6 +10,7 @@ namespace ncaa_grad_info
     {
         public string ChosenConf { get; set; }
 
+        // TOTALS
         public int FED_N_2011_SA { get; set; }
         public int FED_N_2010_SA { get; set; }
         public int FED_N_2009_SA { get; set; }
@@ -44,7 +45,6 @@ namespace ncaa_grad_info
         public int FED_RATE_1997_SA { get; set; }
         public int FED_RATE_1996_SA { get; set; }
         public int FED_RATE_1995_SA { get; set; }
-
         public int GSR_N_2011_SA { get; set; }
         public int GSR_N_2010_SA { get; set; }
         public int GSR_N_2009_SA { get; set; }
@@ -80,7 +80,75 @@ namespace ncaa_grad_info
         public int GSR_1996_SA { get; set; }
         public int GSR_1995_SA { get; set; }
 
-
+        // AVERAGES
+        public int AVG_FED_N_2011_SA { get; set; }
+        public int AVG_FED_N_2010_SA { get; set; }
+        public int AVG_FED_N_2009_SA { get; set; }
+        public int AVG_FED_N_2008_SA { get; set; }
+        public int AVG_FED_N_2007_SA { get; set; }
+        public int AVG_FED_N_2006_SA { get; set; }
+        public int AVG_FED_N_2005_SA { get; set; }
+        public int AVG_FED_N_2004_SA { get; set; }
+        public int AVG_FED_N_2003_SA { get; set; }
+        public int AVG_FED_N_2002_SA { get; set; }
+        public int AVG_FED_N_2001_SA { get; set; }
+        public int AVG_FED_N_2000_SA { get; set; }
+        public int AVG_FED_N_1999_SA { get; set; }
+        public int AVG_FED_N_1998_SA { get; set; }
+        public int AVG_FED_N_1997_SA { get; set; }
+        public int AVG_FED_N_1996_SA { get; set; }
+        public int AVG_FED_N_1995_SA { get; set; }
+        public int AVG_FED_RATE_2011_SA { get; set; }
+        public int AVG_FED_RATE_2010_SA { get; set; }
+        public int AVG_FED_RATE_2009_SA { get; set; }
+        public int AVG_FED_RATE_2008_SA { get; set; }
+        public int AVG_FED_RATE_2007_SA { get; set; }
+        public int AVG_FED_RATE_2006_SA { get; set; }
+        public int AVG_FED_RATE_2005_SA { get; set; }
+        public int AVG_FED_RATE_2004_SA { get; set; }
+        public int AVG_FED_RATE_2003_SA { get; set; }
+        public int AVG_FED_RATE_2002_SA { get; set; }
+        public int AVG_FED_RATE_2001_SA { get; set; }
+        public int AVG_FED_RATE_2000_SA { get; set; }
+        public int AVG_FED_RATE_1999_SA { get; set; }
+        public int AVG_FED_RATE_1998_SA { get; set; }
+        public int AVG_FED_RATE_1997_SA { get; set; }
+        public int AVG_FED_RATE_1996_SA { get; set; }
+        public int AVG_FED_RATE_1995_SA { get; set; }
+        public int AVG_GSR_N_2011_SA { get; set; }
+        public int AVG_GSR_N_2010_SA { get; set; }
+        public int AVG_GSR_N_2009_SA { get; set; }
+        public int AVG_GSR_N_2008_SA { get; set; }
+        public int AVG_GSR_N_2007_SA { get; set; }
+        public int AVG_GSR_N_2006_SA { get; set; }
+        public int AVG_GSR_N_2005_SA { get; set; }
+        public int AVG_GSR_N_2004_SA { get; set; }
+        public int AVG_GSR_N_2003_SA { get; set; }
+        public int AVG_GSR_N_2002_SA { get; set; }
+        public int AVG_GSR_N_2001_SA { get; set; }
+        public int AVG_GSR_N_2000_SA { get; set; }
+        public int AVG_GSR_N_1999_SA { get; set; }
+        public int AVG_GSR_N_1998_SA { get; set; }
+        public int AVG_GSR_N_1997_SA { get; set; }
+        public int AVG_GSR_N_1996_SA { get; set; }
+        public int AVG_GSR_N_1995_SA { get; set; }
+        public int AVG_GSR_2011_SA { get; set; }
+        public int AVG_GSR_2010_SA { get; set; }
+        public int AVG_GSR_2009_SA { get; set; }
+        public int AVG_GSR_2008_SA { get; set; }
+        public int AVG_GSR_2007_SA { get; set; }
+        public int AVG_GSR_2006_SA { get; set; }
+        public int AVG_GSR_2005_SA { get; set; }
+        public int AVG_GSR_2004_SA { get; set; }
+        public int AVG_GSR_2003_SA { get; set; }
+        public int AVG_GSR_2002_SA { get; set; }
+        public int AVG_GSR_2001_SA { get; set; }
+        public int AVG_GSR_2000_SA { get; set; }
+        public int AVG_GSR_1999_SA { get; set; }
+        public int AVG_GSR_1998_SA { get; set; }
+        public int AVG_GSR_1997_SA { get; set; }
+        public int AVG_GSR_1996_SA { get; set; }
+        public int AVG_GSR_1995_SA { get; set; }
 
     }
 }
