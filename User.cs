3ea02995ddf914f4ceccd157fe45c7c7d78f1a2e
J. Nathan Allen <nateonmission@gmail.com>
2019-02-25 19:50:50 -0500

    user obj

---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ncaa_grad_info
+{
+    class User
+    {
+        public string Username { get; set; }
+        public string NameFirst { get; set; }
+        public string NameLast { get; set; }
+        public string FavConf { get; set; }
+    }
+}
